df62562e969e4be93d0f3fc2a9779a2777840a0f
joaoguilhermerp <jaumdalolo@gmail.com>
2022-04-20 10:46:06 -0300

    Add project files.

---
+++ Data/HeroiContext.cs
@@ -0,0 +1,28 @@
+﻿using EFCore.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.WebAPI.Data
+{
+    public class HeroiContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+        public DbSet<HeroiBatalha> HeroiBatalhas { get; set; }
+        public DbSet<IdentidadeSecreta> IdentidadeSecretas { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer( connectionString : @"Integrated Security=SSPI;Persist Security Info=False;User ID=DESKTOP-MVRRLU9\pczin;Initial Catalog=HeroApp;Data Source=DESKTOP-MVRRLU9\SQLEXPRESS");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroiBatalha>(entity =>
+            {
+                entity.HasKey(e => new { e.BatalhaId, e.HeroId });
+            });
+        }
+    }
+}
---
+++ Models/Batalha.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFCore.WebAPI.Models
+{
+    public class Batalha
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DtInicio { get; set; }
+        public DateTime DtFim { get; set; }
+        public List<HeroiBatalha> HeroiBatalhas { get; set; }
+    }
+}
